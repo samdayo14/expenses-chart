70307f3a38cfeea40413c3b84ef428db7377d441
Samuel Adeniran <107911565+samdayo14@users.noreply.github.com>
2023-09-10 06:57:55 -0700

    Update script.js

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,34 +1,35 @@
- $(document).ready(function () {
-  // Fetch the JSON data
-  fetch('data.json')
+
+fetch('data.json')
     .then(function (response) {
-      return response.json();
+        return response.json();
     })
     .then(function (data) {
-      // Select all the day elements with class 'day'
-      var days = document.querySelectorAll('.day');
+        // Select all the day elements with class 'day'
+        var days = document.querySelectorAll('.day');
 
-      // Loop through each day element
-      days.forEach(function (day, index) {
-        // Add a hover event listener
-        day.addEventListener('mouseenter', function () {
-          // Get the amount associated with this day from the loaded JSON data
-          var amount = data[index].amount;
+        // Loop through each day element
+        days.forEach(function (day, index) {
+            // Create a unique identifier for the amount element
+            var amountId = 'amount_' + index;
 
-          // Display the amount
-          var amountElement = document.getElementById('amount');
-          amountElement.textContent = '$' + amount.toFixed(2);
-          amountElement.style.display = 'block';
+            // Add a hover event listener
+            day.addEventListener('mouseenter', function () {
+                // Get the amount associated with this day from the loaded JSON data
+                var amount = data[index].amount;
+
+                // Display the amount
+                var amountElement = document.getElementById(amountId);
+                amountElement.textContent = '$' + amount.toFixed(2);
+                amountElement.style.display = 'block';
+            });
+
+            // Add a mouseleave event listener to hide the amount when not hovering
+            day.addEventListener('mouseleave', function () {
+                var amountElement = document.getElementById(amountId);
+                amountElement.style.display = 'none';
+            });
         });
-
-        // Add a mouseleave event listener to hide the amount when not hovering
-        day.addEventListener('mouseleave', function () {
-          var amountElement = document.getElementById('amount');
-          amountElement.style.display = 'none';
-        });
-      });
     })
     .catch(function (error) {
-      console.error('Error loading JSON data:', error);
+        console.error('Error loading JSON data:', error);
     });
-});
