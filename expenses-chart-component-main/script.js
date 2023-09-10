f57ee7993ed95cf1832beed5d4b2f3f9da6e7e20
Samuel Adeniran <107911565+samdayo14@users.noreply.github.com>
2023-09-10 05:06:28 -0700

    better code of script.js

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,13 +1,34 @@
-const graph = document.querySelectorAll(".num-day");
+ $(document).ready(function () {
+  // Fetch the JSON data
+  fetch('data.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Select all the day elements with class 'day'
+      var days = document.querySelectorAll('.day');
 
-graph.forEach((numDay) => {
-  const amount = numDay.querySelector(".fig");
+      // Loop through each day element
+      days.forEach(function (day, index) {
+        // Add a hover event listener
+        day.addEventListener('mouseenter', function () {
+          // Get the amount associated with this day from the loaded JSON data
+          var amount = data[index].amount;
 
-  numDay.addEventListener("mouseenter", () => {
-    amount.style.display = "block";
-  });
+          // Display the amount
+          var amountElement = document.getElementById('amount');
+          amountElement.textContent = '$' + amount.toFixed(2);
+          amountElement.style.display = 'block';
+        });
 
-  numDay.addEventListener("mouseleave", () => {
-    amount.style.display = "none";
-  });
+        // Add a mouseleave event listener to hide the amount when not hovering
+        day.addEventListener('mouseleave', function () {
+          var amountElement = document.getElementById('amount');
+          amountElement.style.display = 'none';
+        });
+      });
+    })
+    .catch(function (error) {
+      console.error('Error loading JSON data:', error);
+    });
 });
